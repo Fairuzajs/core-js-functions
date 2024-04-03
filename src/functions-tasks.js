813f96fcc557a02323fb27c1a3d3a6cb88c40816
fairuzajs <yaubasarova.93@mail.ru>
2024-04-03 11:08:46 +0500

    feat: add polinum

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,17 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coeff) {
+  return function Polinum(x) {
+    if (coeff.length === 0) {
+      return null;
+    }
+    let result = 0;
+    for (let i = 0; i < coeff.length; i += 1) {
+      result += coeff[i] * x ** (coeff.length - 1 - i);
+    }
+    return result;
+  };
 }
 
 /**
